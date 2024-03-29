96e9787e952239536e2dda5325e2788b92ac2bc8
Lulu Wu <sky110423@gmail.com>
2024-03-29 13:33:14 -0700

    104. Maximum Depth of Binary Tree

---
+++ 104-MaximumDepthofBinaryTree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,87 @@
+import java.util.List;
+import java.util.Scanner;
+import java.lang.Math;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static TreeNode constructBinaryTree(String[] input) {
+        if (input == null || input.length == 0 || input[0].equals("-1"))
+            return null;
+
+        TreeNode[] nodes = new TreeNode[input.length];
+        for (int i = 0; i < input.length; i++) {
+            if (!input[i].equals("-1")) {
+                nodes[i] = new TreeNode(Integer.parseInt(input[i]));
+            }
+        }
+
+        int leftIndex = 1;
+        int rightIndex = 2;
+
+        for (int i = 0; i < input.length; i++) {
+            if (nodes[i] != null) {
+                if (leftIndex < input.length) {
+                    nodes[i].left = nodes[leftIndex];
+                }
+                if (rightIndex < input.length) {
+                    nodes[i].right = nodes[rightIndex];
+                }
+                leftIndex += 2;
+                rightIndex += 2;
+            }
+
+        }
+
+        return nodes[0];
+    }
+
+    public static void main(String[] args) {
+        // declaration
+        MaximumDepthofBinaryTree solution = new MaximumDepthofBinaryTree();
+        Scanner in = new Scanner(System.in);
+        TreeNode root;
+        int result;
+
+        // input
+        System.out.println(
+                "\nEnter the values to construct the binary tree (separated by spaces, enter -1 for null nodes):");
+        System.out.print("> ");
+        String[] input = in.nextLine().split(" ");
+        root = constructBinaryTree(input);
+
+        // processing
+        result = solution.maxDepth(root);
+
+        // output
+        System.out.println("\nResult:\n");
+        System.out.printf("The depth of the tree is %d\n\n", result);
+    }
+}
